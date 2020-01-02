0dda45733c8cfa9a22c56318b51c97b00ce11e3d
lrh-l <1538906305@qq.com>
2020-01-02 17:04:03 +0800

    分支

--- src/main/java/com/lrh/cs/test/Test7.java
+++ src/main/java/com/lrh/cs/test/Test7.java
@@ -1,8 +1,13 @@
 package com.lrh.cs.test;
 
 public class Test7 {
-    private  static Test7 test7 =null;
+    private  static Test7 test7 ;
     private Test7(){}
-
+    public static synchronized Test7 getTest7(){
+        if (test7 == null) {
+            test7 = new Test7();
+        }
+        return test7;
+    }
 
 }
